48453509b744e67e1dee5206e756f346d13f346d
XSTStone <57900292+XSTStone@users.noreply.github.com>
2024-10-09 11:06:41 +0800

    Oct_9

--- TDeepSC/execute.sh
+++ TDeepSC/execute.sh
@@ -9,4 +9,7 @@ CUDA_VISIBLE_DEVICES=2 python3  tdeepsc_main.py \
     --save_freq 2   \
     --ta_perform vqa  
   
-   
+
+
+CUDA_VISIBLE_DEVICES=2 python3  tdeepsc_main.py --model  TDeepSC_vqa_model  --output_dir ckpt_record --batch_size 30 --input_size 224 --lr  1e-4 --epochs 200  --opt_betas 0.95 0.99  --save_freq 2  --ta_perform vqa
+
--- UDeepSC/execute.sh
+++ UDeepSC/execute.sh
@@ -1,17 +1,4 @@
-CUDA_VISIBLE_DEVICES=1  python3  udeepsc_main.py \
-    --model  UDeepSC_new_model  \
-    --output_dir ckpt_record_12dB_single   \
-    --batch_size 16 \
-    --input_size 32 \
-    --lr  6e-6 \
-    --epochs 500  \
-    --opt_betas 0.95 0.99  \
-    --save_freq 2   \
-    --ta_perform textr \
-    --resume ckpt_record_12dB_single/ckpt_msa/checkpoint-203.pth\
-    --eval
-   
-  
+CUDA_VISIBLE_DEVICES=1  python3  udeepsc_main.py --model  UDeepSC_new_model --output_dir ckpt_record_12dB_single --batch_size 16 --input_size 32 --lr  6e-6 --epochs 500  --opt_betas 0.95 0.99 --save_freq 2 --ta_perform textr --resume ckpt_record_12dB_single/ckpt_msa/checkpoint-203.pth --eval
 
    
 
