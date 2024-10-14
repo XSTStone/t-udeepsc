490f5267719f94a18757e55eaa07b8fccdd1db26
XSTStone <57900292+XSTStone@users.noreply.github.com>
2024-10-14 20:46:32 +0800

    更改execute.sh的内容

--- TDeepSC/execute.sh
+++ TDeepSC/execute.sh
@@ -14,3 +14,5 @@ CUDA_VISIBLE_DEVICES=2 python3  tdeepsc_main.py \
 CUDA_VISIBLE_DEVICES=1 python3  tdeepsc_main.py --model  TDeepSC_textc_model  --output_dir ckpt_record --batch_size 40 --input_size 32 --lr  3e-5 --epochs 40  --opt_betas 0.95 0.99  --save_freq 2  --ta_perform textc
 
 CUDA_VISIBLE_DEVICES=1 python3  tdeepsc_main.py --model  TDeepSC_vqa_model  --output_dir ckpt_record  --batch_size 30 --input_size 224 --lr  1e-4 --epochs 200  --opt_betas 0.95 0.99  --save_freq 2    --ta_perform vqa
+
+CUDA_VISIBLE_DEVICES=1 python3  tdeepsc_main.py --model  TDeepSC_textr_model --output_dir ckpt_record --batch_size 30 --input_size 32 --lr  3e-5 --epochs 20 --opt_betas 0.95 0.99  --save_freq 2 --ta_perform textr --eval
